57abd11ea0af4da0c1c0dc2a03b299e2b53958aa
ishansugathadasa <30472153+ishansugathadasa@users.noreply.github.com>
2018-09-15 11:56:24 +0530

    add login and register

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegisterPage } from '../register/register';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,10 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  onGoToLogin(){
+   this.navCtrl.push(LoginPage);     
+  }
+  onGoToRegister(){
+    this.navCtrl.push(RegisterPage);
+  }
 }
